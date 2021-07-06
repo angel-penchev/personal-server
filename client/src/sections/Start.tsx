54ab4bf659012de42907c941499d67a2520ce53e
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-07-06 20:50:03 +0300

    [Feat] Start portrait
    Added portrait to the start section.

--- client/src/sections/Start.tsx
+++ client/src/sections/Start.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from 'react';
+import portrait from '../assets/portrait.png';
 
 const Start = () => {
     return (
         <div className="start section">
-            <div className="caption">
-                <span className="primary">
-                    Hi, my name is Angel Penchev<br />
-                    and I'm a software developer
-                </span>
-                <span className="secondary"></span>
+            <div className="left-container">
+                <div className="caption">
+                    <span className="primary">
+                        Hi, my name is Angel Penchev<br />
+                        and I'm a software developer
+                    </span>
+                    <span className="secondary">
+                        
+                    </span>
+                </div>
             </div>
-            <div className="portrait">
-                <img src="" alt="" />
+            <div className="right-container">
+                <img className="portrait" src={portrait} alt="Portrait of Angel Penchev" />
             </div>
         </div>
     )
