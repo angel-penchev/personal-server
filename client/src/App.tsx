afcedc71ee319e5ee18411fb20f126738f03a673
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-07-06 16:48:03 +0300

    [Feat] Page section templates
    Created the basic structure of the app in App.tsx.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Awards from './sections/Awards';
+import Contact from './sections/Contact';
+import Education from './sections/Education';
+import Experience from './sections/Experience';
+import Footer from './sections/Footer';
+import Header from './sections/Header';
+import Projects from './sections/Projects';
+import Skills from './sections/Skills';
+import Start from './sections/Start';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <Header></Header>
       </header>
+      <main className="app-main">
+        <Start></Start>
+        <Skills></Skills>
+        <Projects></Projects>
+        <Experience></Experience>
+        <Education></Education>
+        <Awards></Awards>
+        <Contact></Contact>
+      </main>
+      <footer className="app-footer">
+        <Footer></Footer>
+      </footer>
     </div>
   );
 }
