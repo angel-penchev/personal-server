9613e8b47c4f5fe965554eac708aa2dba00a5706
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-07-10 01:12:30 +0300

    [Feat] API public resources
    Uploaded .pdf certificates and CV.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import multiparty from 'multiparty';
+import dotenv from 'dotenv';
 
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+app.use(express.static('public'));
+app.use('/public', express.static(__dirname + '/../public'));
+console.log(__dirname);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
 
-app.listen(port, () => {
-  return console.log(`Server is listening on ${port}...`);
-});
