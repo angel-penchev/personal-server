8b6e7b93df6eb8dbcfbc94dc4dff599e9ea8a24f
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-07-14 13:47:44 +0300

    [Feat] Map with applied theme + Assets move
    Added google maps which shows I'm based in Sofia, Bulgaria.

---
+++ client/src/components/MapMarker/MapMarker.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './MapMarker.css';
+import marker from '../../assets/map-marker.svg';
+
+interface Props {
+    lat?: number;
+    lng?: number;
+}
+
+const MapMarker = (props: Props) => {
+  return (
+    <div className="marker">
+      <img
+        src={marker}
+        alt="Map location marker" />
+    </div>
+  );
+};
+
+export default MapMarker;
--- client/src/sections/Contact/Contact.tsx
+++ client/src/sections/Contact/Contact.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Contact.css';
+import GoogleMap from 'google-map-react';
+import MapTheme from '../../assets/map-theme.json';
+import MapMarker from '../../components/MapMarker/MapMarker';
+
+const mapCenter = {lat: 42.6978634, lng: 23.3221789};
+const mapPin = {lat: 42.6978634, lng: 23.3221789};
+const mapZoom = 6;
 
 const Contact = () => {
   return (
     <div className="contact section">
-
+      <div className="map">
+        <GoogleMap
+          bootstrapURLKeys={{key: String(process.env.GOOGLE_MAPS_API_KEY)}}
+          defaultCenter={mapCenter}
+          defaultZoom={mapZoom}
+          options={{
+            styles: MapTheme,
+            gestureHandling: 'greedy',
+            disableDefaultUI: true,
+          }}
+        >
+          <MapMarker
+            lat={mapPin.lat}
+            lng={mapPin.lng}
+          />
+        </GoogleMap>
+      </div>
     </div>
   );
 };
