d344f7311aa66c5d48d65b357cfd15e77860ae2b
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-07-14 15:37:14 +0300

    [Feat] Contact section layout
    Created basic contact section layout for desktop.

--- client/src/sections/Contact/Contact.tsx
+++ client/src/sections/Contact/Contact.tsx
@@ -4,13 +4,20 @@ import GoogleMap from 'google-map-react';
 import MapTheme from '../../assets/misc/map-theme.json';
 import MapMarker from '../../components/MapMarker/MapMarker';
 
-const mapCenter = {lat: 42.6978634, lng: 23.3221789};
+const mapCenter = {lat: 44.7629166, lng: 21.5161946};
 const mapPin = {lat: 42.6978634, lng: 23.3221789};
 const mapZoom = 6;
 
 const Contact = () => {
   return (
     <div className="contact section">
+      <div className="section-heading">
+        <h2 className="heading">Contact</h2>
+        <h3 className="subheading">
+          This page is still under construction. However, you can still contact
+          me using the form below.
+        </h3>
+      </div>
       <div className="map">
         <GoogleMap
           bootstrapURLKeys={{
@@ -30,6 +37,8 @@ const Contact = () => {
           />
         </GoogleMap>
       </div>
+      <div className="form"></div>
+      <div className="information"></div>
     </div>
   );
 };
